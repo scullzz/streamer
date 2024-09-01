31ce6eececdcb6cc951fd2aa4c6c9ae99ba66b1d
scullzz <rasulhuzaevsaid@gmail.com>
2024-09-01 20:24:49 +0500

    fix list

--- src/components/widget_gs/Gs_Widget.tsx
+++
@@ -1,8 +0,0 @@
-import { GlobalPay } from "global-solutions-widget";
-const Gs_Widget = () => {
-  const widget = new GlobalPay();
-  widget.showWidget();
-  return <div></div>;
-};
-
-export default Gs_Widget;
