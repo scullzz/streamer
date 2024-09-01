9a0188cc1cd9537b95e07aef6cda2adc44e6c0f4
scullzz <rasulhuzaevsaid@gmail.com>
2024-09-01 20:58:59 +0500

    fix list

--- src/components/live_streamers/LiveStreamers.tsx
+++ src/components/live_streamers/LiveStreamers.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reply from "./image/reply.svg";
 import searchIcon from "./image/search.svg";
 import style from "./style.module.css";
@@ -19,115 +19,114 @@ import LiveStreamerItem from "../live_streamers_item/LiveStreamerItem";
 const LiveStreamers = () => {
   const [search, setSearch] = useState<null | string>(null);
   console.log(search);
+
   return (
-    <div className={style.section}>
-      <div className={`${style.LiveStreamers}`}>
-        <div className={style.searchContainer}>
-          <img src={searchIcon} alt="#" />
-          <input
-            type="text"
-            className={style.searchInput}
-            placeholder="Поиск"
-            onChange={(e) => setSearch(e.currentTarget.value)}
-          />
-        </div>
-
-        <div className={style.StreamersListHeader}>
-          <span>Стримеры</span>
-          <img src={reply} alt="#" />
-        </div>
-
-        <LiveStreamerItem
-          is_subscribed={true}
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-        ></LiveStreamerItem>
-        <LiveStreamerItem
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-          is_subscribed={true}
-        ></LiveStreamerItem>
-        <LiveStreamerItem
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-          is_subscribed={false}
-        ></LiveStreamerItem>
-        <LiveStreamerItem
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-          is_subscribed={true}
-        ></LiveStreamerItem>
-        <LiveStreamerItem
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-          is_subscribed={false}
-        ></LiveStreamerItem>
-        <LiveStreamerItem
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-          is_subscribed={false}
-        ></LiveStreamerItem>
-        <LiveStreamerItem
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-          is_subscribed={false}
-        ></LiveStreamerItem>
-        <LiveStreamerItem
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-          is_subscribed={false}
-        ></LiveStreamerItem>
-        <LiveStreamerItem
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-          is_subscribed={false}
-        ></LiveStreamerItem>
-        <LiveStreamerItem
-          imgUrl={""}
-          name={""}
-          subscribers={0}
-          youtubeOnline={0}
-          twitchOnline={0}
-          kickOnline={0}
-          is_subscribed={false}
-        ></LiveStreamerItem>
+    <div className={style.LiveStreamers}>
+      <div className={style.searchContainer}>
+        <img src={searchIcon} alt="#" />
+        <input
+          type="text"
+          className={style.searchInput}
+          placeholder="Поиск"
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
       </div>
+
+      <div className={style.StreamersListHeader}>
+        <span>Стримеры</span>
+        <img src={reply} alt="#" />
+      </div>
+
+      <LiveStreamerItem
+        is_subscribed={true}
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+      ></LiveStreamerItem>
+      <LiveStreamerItem
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+        is_subscribed={true}
+      ></LiveStreamerItem>
+      <LiveStreamerItem
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+        is_subscribed={false}
+      ></LiveStreamerItem>
+      <LiveStreamerItem
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+        is_subscribed={true}
+      ></LiveStreamerItem>
+      <LiveStreamerItem
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+        is_subscribed={false}
+      ></LiveStreamerItem>
+      <LiveStreamerItem
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+        is_subscribed={false}
+      ></LiveStreamerItem>
+      <LiveStreamerItem
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+        is_subscribed={false}
+      ></LiveStreamerItem>
+      <LiveStreamerItem
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+        is_subscribed={false}
+      ></LiveStreamerItem>
+      <LiveStreamerItem
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+        is_subscribed={false}
+      ></LiveStreamerItem>
+      <LiveStreamerItem
+        imgUrl={""}
+        name={""}
+        subscribers={0}
+        youtubeOnline={0}
+        twitchOnline={0}
+        kickOnline={0}
+        is_subscribed={false}
+      ></LiveStreamerItem>
     </div>
   );
 };
