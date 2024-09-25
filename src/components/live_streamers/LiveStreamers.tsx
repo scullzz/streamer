299c9c6eb12f8a259c45d898ce9b121ae0db26d0
scullzz <rasulhuzaevsaid@gmail.com>
2024-09-26 00:00:44 +0500

    fix

--- src/components/live_streamers/LiveStreamers.tsx
+++ src/components/live_streamers/LiveStreamers.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchListOfLiveStreamers,
