ba55fe9381c9011abd36068117d8f7eb49f66fcd
Saidazam Rasulhujaev <rasulhuzaevsaid@gmail.com>
2024-10-04 01:32:06 +0500

    init

--- src/components/view_online/ViewOnline.tsx
+++ src/components/view_online/ViewOnline.tsx
@@ -38,41 +38,42 @@ const ViewOnline = () => {
     getImage();
   }, []);
   return (
-    <div className={styles.back}>
+    <>
       <SectionHeader
         left={<span onClick={() => backPage()}>Назад</span>}
         center={<span style={{ color: "white" }}>Clash of Slots</span>}
       />
-      <div className={styles.streamContainer}>
-        <div className={styles.streamInfo}>
-          <iframe
-            width="100%"
-            height="210px"
-            src={youtubeEmbedLink}
-            title={title}
-            className={styles.streamThumbnail}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
-      </div>
-
-      <div className={styles.buttonGroup}>
-        <button
-          onClick={() => {
-            window.location.href = link;
-          }}
-          className={styles.twitchButton}
-        >
-          <div className={styles.buttonblock}>
-            <span className={`${styles[selectedStyle]}`}>{viewers}</span>
-            <span> Смотреть на {platform}</span>
+      <div className={styles.back}>
+        <div className={styles.streamContainer}>
+          <div className={styles.streamInfo}>
+            <iframe
+              width="100%"
+              height="210px"
+              src={youtubeEmbedLink}
+              title={title}
+              className={styles.streamThumbnail}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
-        </button>
-      </div>
+        </div>
 
-      {/* <div className={styles.chatContainer}>
+        <div className={styles.buttonGroup}>
+          <button
+            onClick={() => {
+              window.location.href = link;
+            }}
+            className={styles.twitchButton}
+          >
+            <div className={styles.buttonblock}>
+              <span className={`${styles[selectedStyle]}`}>{viewers}</span>
+              <span> Смотреть на {platform}</span>
+            </div>
+          </button>
+        </div>
+
+        {/* <div className={styles.chatContainer}>
         <div className={styles.chatHeader}>Чат</div>
         <div className={styles.chatMessages}>
           <div className={styles.chatInput}>
@@ -81,24 +82,27 @@ const ViewOnline = () => {
         </div>
       </div> */}
 
-      <div className={styles.ratingContainer}>
-        <h3 className={styles.p3_changed}>Рейтинг онлайн казино</h3>
-        <div className={styles.casinoList}>
-          <div className={styles.casinoItem}>
-            <div className={styles.miniFlex}>
-              <div className={styles.casinoLogo}>
-                <img src={trash} alt="Auf Casino" />
+        <div className={styles.ratingContainer}>
+          <h3 className={styles.p3_changed}>Рейтинг онлайн казино</h3>
+          <div className={styles.casinoList}>
+            <div className={styles.casinoItem}>
+              <div className={styles.miniFlex}>
+                <div className={styles.casinoLogo}>
+                  <img src={trash} alt="Auf Casino" />
+                </div>
+                <div className={styles.casinoInfo}>
+                  <span className={styles.casinoMainText}>Auf Casino</span>
+                  <span className={styles.casinoMiniText}>
+                    Промокод: COBRIK
+                  </span>
+                </div>
               </div>
-              <div className={styles.casinoInfo}>
-                <span className={styles.casinoMainText}>Auf Casino</span>
-                <span className={styles.casinoMiniText}>Промокод: COBRIK</span>
-              </div>
+              <button className={styles.playButton}>Играть</button>
             </div>
-            <button className={styles.playButton}>Играть</button>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
