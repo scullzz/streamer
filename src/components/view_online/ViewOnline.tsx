36f6f80c2b60afbd124ddf083e8fbca36647ec15
Saidazam Rasulhujaev <rasulhuzaevsaid@gmail.com>
2024-11-08 01:50:24 +0500

    swipe

--- src/components/view_online/ViewOnline.tsx
+++ src/components/view_online/ViewOnline.tsx
@@ -192,6 +192,7 @@ const ViewOnline = () => {
           onClose={() => setPanelPosition("closed")}
           onOpen={() => setPanelPosition("half")}
           disableSwipeToOpen={true}
+          disableBackdropTransition
           swipeAreaWidth={0}
           sx={{
             "& .MuiDrawer-paper": {
